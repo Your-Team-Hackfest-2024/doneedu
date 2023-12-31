ed88cdc3f11b5e16a27c42059d81d5153af356b9
mrevanzak <mrevanzak@gmail.com>
2023-12-31 19:27:08 +0700

    chore: Remove unused media driver configuration

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,6 +1,5 @@
 import { createAnimations } from '@tamagui/animations-react-native';
 import { createInterFont } from '@tamagui/font-inter';
-import { createMedia } from '@tamagui/react-native-media-driver';
 import { shorthands } from '@tamagui/shorthands';
 import { themes, tokens } from '@tamagui/themes';
 import { createTamagui } from 'tamagui';
@@ -118,22 +117,22 @@ const config = createTamagui({
     },
   },
   tokens,
-  media: createMedia({
-    xs: { maxWidth: 660 },
-    sm: { maxWidth: 800 },
-    md: { maxWidth: 1020 },
-    lg: { maxWidth: 1280 },
-    xl: { maxWidth: 1420 },
-    xxl: { maxWidth: 1600 },
-    gtXs: { minWidth: 660 + 1 },
-    gtSm: { minWidth: 800 + 1 },
-    gtMd: { minWidth: 1020 + 1 },
-    gtLg: { minWidth: 1280 + 1 },
-    short: { maxHeight: 820 },
-    tall: { minHeight: 820 },
-    hoverNone: { hover: 'none' },
-    pointerCoarse: { pointer: 'coarse' },
-  }),
+  // media: createMedia({
+  //   xs: { maxWidth: 660 },
+  //   sm: { maxWidth: 800 },
+  //   md: { maxWidth: 1020 },
+  //   lg: { maxWidth: 1280 },
+  //   xl: { maxWidth: 1420 },
+  //   xxl: { maxWidth: 1600 },
+  //   gtXs: { minWidth: 660 + 1 },
+  //   gtSm: { minWidth: 800 + 1 },
+  //   gtMd: { minWidth: 1020 + 1 },
+  //   gtLg: { minWidth: 1280 + 1 },
+  //   short: { maxHeight: 820 },
+  //   tall: { minHeight: 820 },
+  //   hoverNone: { hover: 'none' },
+  //   pointerCoarse: { pointer: 'coarse' },
+  // }),
 });
 
 type AppConfig = typeof config;
