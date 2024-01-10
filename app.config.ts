eba3d22fcb533b31f92486bd6a2fc9abe1fc311d
mrevanzak <mrevanzak@gmail.com>
2024-01-10 21:25:21 +0700

    chore: eas config

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,7 @@ const defineConfig = (): ExpoConfig => ({
   name: 'DoNeedU',
   slug: 'do-need-u',
   version: process.env.VERSION?.toString(),
+  owner: 'your_team',
 
   scheme: 'do-need-u',
   web: {
@@ -46,6 +47,25 @@ const defineConfig = (): ExpoConfig => ({
       foregroundImage: './assets/adaptive-icon.png',
       backgroundColor: '#faf8fc',
     },
+  },
+  extra: {
+    eas: {
+      projectId: 'e5823884-b1d7-4210-b56f-2372c63105f2',
+    },
+    EXPO_PUBLIC_FIREBASE_API_KEY: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    EXPO_PUBLIC_DATABASE_URL: process.env.EXPO_PUBLIC_DATABASE_URL,
+    EXPO_PUBLIC_PROJECT_ID: process.env.EXPO_PUBLIC_PROJECT_ID,
+    EXPO_PUBLIC_STORAGE_BUCKET: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+    EXPO_PUBLIC_MESSAGING_SENDER_ID: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+    EXPO_PUBLIC_APP_ID: process.env.EXPO_PUBLIC_APP_ID,
+    EXPO_PUBLIC_MEASUREMENT_ID: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
+  },
+  updates: {
+    url: 'https://u.expo.dev/e5823884-b1d7-4210-b56f-2372c63105f2',
+  },
+  runtimeVersion: {
+    policy: 'appVersion',
   },
 });
 
